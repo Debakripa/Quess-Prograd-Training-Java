bf4a233b403bcb72b26fc80ec6ab1fd36b62f180
Debakripa <54179159+Debakripa@users.noreply.github.com>
2022-07-12 22:56:01 +0530

    Q19 - Checks if given integer is Armstrong Number

---
+++ ArmstrongNumber.java
@@ -0,0 +1,34 @@
+import java.util.*;
+public class ArmstrongNumber {
+    public static void main(String[] args) {
+        System.out.println("Enter a number: ");
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int sum = 0;
+        int temp = num;
+        String number = Integer.toString(temp);
+        int len = number.length();
+        while (num > 0)
+        {
+            len = number.length();
+            int rem = num % 10;
+            int value = 1;
+            while (len > 0)
+            {
+                value *= rem;
+                len--;
+            }
+            //System.out.println(value);
+            sum += value;
+            num /= 10;
+        }
+        if (sum == temp)
+        {
+            System.out.println("Armstrong");
+        }
+        else
+        {
+            System.out.println("Not Armstrong");
+        }
+    }
+}
