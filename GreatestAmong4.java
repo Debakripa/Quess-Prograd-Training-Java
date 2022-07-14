df1c1ebe4d5eb61d0f02a6c56044c70e7a1c1523
Debakripa <54179159+Debakripa@users.noreply.github.com>
2022-07-14 10:24:39 +0530

    Output highest of 4 real numbers
    Output maximum number among 4 real numbers, using ternary operator

---
+++ GreatestAmong4.java
@@ -0,0 +1,20 @@
+//Output the greatest among 4 numbers entered
+import java.util.*;
+public class GreatestAmong4
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Enter 4 numbers: ");
+        Scanner sc = new Scanner(System.in);
+        double num1 = sc.nextDouble();
+        double num2 = sc.nextDouble();
+        double num3 = sc.nextDouble();
+        double num4 = sc.nextDouble();
+        double max1,max2,max;
+        max1 = (num1>num2)?num1:num2;
+        max2 = (num3>num4)?num3:num4;
+        max = (max1>max2)?max1:max2;
+        System.out.println("Greatest number is "+max);
+
+    }
+}
